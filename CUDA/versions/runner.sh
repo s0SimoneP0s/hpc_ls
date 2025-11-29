3991360e6f0840805561864a03f878df88e00fe8
spellacani <53236@studenti.unimore.it>
2025-11-29 14:31:24 +0000

    firxed runner

--- CUDA/versions/runner.sh
+++ CUDA/versions/runner.sh
@@ -2,8 +2,6 @@
 
 # Stampa intestazione CSV una sola volta
 echo "dataset,n,tsteps,cpu_threads,block_size,seconds_time_elapsed,insn_per_cycle,branch_misses,gpu_teams,gpu_threads_per_team"
-
-echo
 cd jacobi-1d-imper_es
 ./plotter.sh
 cd ..
