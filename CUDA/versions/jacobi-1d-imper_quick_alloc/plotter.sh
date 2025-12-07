7fbce20243d95ff1afe480718516c50652f4c15d
spellacani <53236@studenti.unimore.it>
2025-12-07 10:33:14 +0000

    fix parenthesis

--- CUDA/versions/jacobi-1d-imper_quick_alloc/plotter.sh
+++ CUDA/versions/jacobi-1d-imper_quick_alloc/plotter.sh
@@ -5,7 +5,7 @@ source ../../utilities/gpu_check.sh
 
 declare -a test_size_list=("test_mini_CU_qa" "test_small_CU_qa" "test_standard_CU_qa" "test_large_CU_qa" "test_extralarge_CU_qa")
 
-if [ $1 == "debug" ] ; then
+if [[ $1 == "debug" ]] ; then
     check_gpu
 fi
 
