5577bd88f78f3fc1cfa141ea691d326a2a799dd1
spellacani <53236@studenti.unimore.it>
2025-12-07 10:55:52 +0000

    removed couter value

--- CUDA/versions/jacobi-1d-imper_quick_alloc/jacobi-1d-imper.cu
+++ CUDA/versions/jacobi-1d-imper_quick_alloc/jacobi-1d-imper.cu
@@ -67,17 +67,15 @@ void kernel_jacobi_1d_imper(int tsteps, int n,
   dim3 numThreads(BLOCK_SIZE);
   int numBlocks = (n + BLOCK_SIZE - 1) / BLOCK_SIZE;
 
-  int print=0;
-
   for (int t = 0; t < tsteps; t++) {
 
     start_timer();
     jacobi_1d_kernel<<<numBlocks, numThreads>>>(A, B, n);
     stop_timer();
-    if (print==tsteps/2) //took the middle iteration
+    if (t == tsteps/2) // took the middle iteration
       print_elapsed_ms("SAXPY execution time");
     myCudaMemcpy<<<numBlocks, numThreads>>>(A, B, n);
-    
+
   }
 }
 
